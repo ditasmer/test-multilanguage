27a69407c0911d4b5a7746b4c49fb2bff28bef70
ditasmer <ditasmer@gmail.com>
2020-07-30 12:09:06 +0200

    Protección cambio a idioma no permitido

--- multilanguage.php
+++ multilanguage.php
@@ -1,12 +1,20 @@
 <!--DOCUMENTO 1 con el section del Doc1-->
 <?php
+//array de idiomas permitidos
+$array_idiomas = ['es', 'ca'];
+
 	//definir un idioma por defecto
 	$idioma = 'es';
 
 	//definir el idioma por seleccion del usuario
 	//peticiones que llegan por URL son GET SIEMPRE
 	if(isset($_GET['idioma'])){
-		$idioma = $_GET['idioma'];
+		//verifica que es idioma permitido, si no existe deja el idioma por defecto = es
+		//busca contenido "x" en array escalar 
+		if(in_array($_GET['idioma'], $array_idiomas)){
+			$idioma = $_GET['idioma'];	
+		}
+		
 	}
 	//include de idioma seleccionado entre ""
 	include("multiidioma/contenido_$idioma.php");
